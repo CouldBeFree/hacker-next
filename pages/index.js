4a4b3e8351ca11166f46691b07830a2c4c948c95
Sasha Zoria <alex.zoria@n3wnormal.com>
2019-02-25 11:49:53 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+class Index extends React.Component {
+    render() {
+        return (
+            <div>
+                I am index component
+            </div>
+        )
+    }
+}
+
+export default Index;
