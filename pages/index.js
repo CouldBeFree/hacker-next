c7e48f8da38b618d200ca3fe043f8da9f7a65888
Sasha Zoria <alex.zoria@n3wnormal.com>
2019-02-25 12:19:13 +0200

    Get data from API/Handling errors

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,38 @@
+import fetch from 'isomorphic-fetch';
+import Error from 'next/error';
+
 class Index extends React.Component {
+    static async getInitialProps () {
+        let stories;
+        try {
+            const response = await fetch (
+                "https://node-hnapi.herokuapp.com/news?page=1"
+            );
+            stories = await response.json();
+        }
+        catch (err){
+            console.log(err);
+            stories = [];
+        }
+
+        return {stories};
+    }
+
     render() {
+        const { stories } = this.props;
+
+        if(stories.length === 0) {
+            return <Error statusCode={503} />
+        }
+
         return (
             <div>
-                I am index component
+                <h1>Hacker next</h1>
+                <div>
+                    {stories.map(story => (
+                        <h2 key={story.id}>{story.title}</h2>
+                    ))}
+                </div>
             </div>
         )
     }
